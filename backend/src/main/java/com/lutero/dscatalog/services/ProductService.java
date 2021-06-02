063c59650ff788a6b732be926452102003118c6e
Marcos Lutero da Silva Rocha <lutero_rocha@outlook.com>
2021-06-02 15:07:51 -0300

    Product repository. service. resource. GET ok

---
+++ backend/src/main/java/com/lutero/dscatalog/services/ProductService.java
@@ -0,0 +1,91 @@
+package com.lutero.dscatalog.services;
+
+
+import java.util.Optional;
+
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lutero.dscatalog.dto.ProductDTO;
+import com.lutero.dscatalog.entities.Product;
+import com.lutero.dscatalog.repositories.ProductRepository;
+import com.lutero.dscatalog.services.exceptions.DatabaseException;
+import com.lutero.dscatalog.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Product> list = repository.findAll(pageRequest);
+		return list.map(x -> new ProductDTO(x));
+	}
+	
+	
+	/*public List<ProductDTO> findAll() {
+		List<Product> list = repository.findAll();
+		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+*/
+		/*
+		 * List <ProductDTO> listDto = new ArrayList<>(); for (Product cat: list){
+		 * listDto.add(new Product(cat)) } return listDto;
+		 *
+		 
+
+	}
+	*/
+
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Optional<Product> obj = repository.findById(id); // pego o objeto do optional
+		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not Found")); // agora sim e a
+																									// entidade
+		return new ProductDTO(entity, entity.getCategories()); // agora ela vai retornar no dto
+	}
+
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		//entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+
+	}
+
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		try {
+			Product entity = repository.getOne(id);
+			//entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id Not Found" + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException("Id Not Foud" + id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrety violation");
+			
+			
+		}
+		
+	}
+
+}
